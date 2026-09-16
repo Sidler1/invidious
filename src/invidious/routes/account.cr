44a31681fa40005378f06e6d32ce623b75f104ea
Felix Schmäche <fschmaeche@schmaeche.org>
2026-09-16 13:45:42 +0200

    fix: escape callback origin in token form and enforce http(s) callback at the redirect sink

--- src/invidious/routes/account.cr
+++ src/invidious/routes/account.cr
@@ -250,6 +250,10 @@ module Invidious::Routes::Account
       access_token = URI.encode_www_form(access_token)
       url = URI.parse(callback_url)
 
+      if !{"http", "https"}.includes?(url.scheme)
+        return error_template(400, "Invalid callback URL")
+      end
+
       if url.query
         query = HTTP::Params.parse(url.query.not_nil!)
       else
